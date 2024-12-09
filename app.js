fe6b246ee6a15627ac3c2bc8e5ad5b1e2697feed
Eugene <eugenepomevor@gmail.com>
2024-12-09 00:27:21 +0000

    third push

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is my first node.js deployment using AWS CodePipeline!'));
+app.get('/', (req, res) => res.send('This is my first bloody node.js deployment using AWS CodePipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
